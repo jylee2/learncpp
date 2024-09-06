2ec8e4f22acb6b93372270b3a95f6a22236a2e09
Jun <leej1025y@live.com>
2024-09-06 22:21:43 +1000

    feat: hello world

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment
+    /*
+        This is a multi-line comment
+    */
+
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << '\n';
+    return 0;
+}
